4391db1084fdcbbf49192fef492c1884a91bea16
David W. Hogg <dwhogg@gmail.com>
2012-08-01 08:30:38 +0200

    add circle areas

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
+"""
+This file is part of the flask+d3 Hello World project.
+"""
 import json
-
 import flask
 import numpy as np
 
-
+# @dfm what does this next line do?
 app = flask.Flask(__name__)
 
-
+# @dfm what is the @app.route() doing?
 @app.route("/")
 def index():
+    """
+    @dfm:  What does this do?
+    """
     return flask.render_template("index.html")
-
 
 @app.route("/data")
 @app.route("/data/<int:ndata>")
-def data(ndata=10):
+def data(ndata=100):
+    """
+    On request, this returns a list of ndata randomly made data points.
+    """
     x = 10 * np.random.rand(ndata) - 5
-    y = 0.5 * x + 2 * np.random.randn(ndata)
-    return json.dumps([{"x": x[i], "y": y[i]} for i in range(ndata)])
+    y = 0.5 * x + 0.5 * np.random.randn(ndata)
+    A = 10. ** np.random.rand(ndata)
+    return json.dumps([{"x": x[i], "y": y[i], "area": A[i]} for i in range(ndata)])
 
-
-if __name__ == "__main__":
-    import os
+def main():
+    """
+    This function opens port 4000 and does what?  @dfm??
+    """
     port = 4000
     os.system("open http://localhost:{0}".format(port))
     app.debug = True
     app.run(port=port)
+
+if __name__ == "__main__":
+    import os
+    main()
