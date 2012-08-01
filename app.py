6b4f2201b9588546d5ccc52b8cccf5e7dc9eba09
Dan F-M <danfm@nyu.edu>
2012-08-01 09:50:18 +0200

    trying to make tooltips and failing

--- app.py
+++ app.py
@@ -5,37 +5,47 @@ import json
 import flask
 import numpy as np
 
-# @dfm what does this next line do?
+
 app = flask.Flask(__name__)
 
-# @dfm what is the @app.route() doing?
+
 @app.route("/")
 def index():
     """
-    @dfm:  What does this do?
+    When you request the root path, you'll get the index.html template.
+
     """
     return flask.render_template("index.html")
+
 
 @app.route("/data")
 @app.route("/data/<int:ndata>")
 def data(ndata=100):
     """
-    On request, this returns a list of ndata randomly made data points.
+    On request, this returns a list of ``ndata`` randomly made data points.
+
+    :param ndata: (optional)
+        The number of data points to return.
+
+    :returns data:
+        A JSON string of ``ndata`` data points.
+
     """
     x = 10 * np.random.rand(ndata) - 5
     y = 0.5 * x + 0.5 * np.random.randn(ndata)
     A = 10. ** np.random.rand(ndata)
-    return json.dumps([{"x": x[i], "y": y[i], "area": A[i]} for i in range(ndata)])
+    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i]}
+        for i in range(ndata)])
 
-def main():
-    """
-    This function opens port 4000 and does what?  @dfm??
-    """
-    port = 4000
-    os.system("open http://localhost:{0}".format(port))
-    app.debug = True
-    app.run(port=port)
 
 if __name__ == "__main__":
     import os
-    main()
+
+    port = 8000
+
+    # Open a web browser pointing at the app.
+    os.system("open http://localhost:{0}".format(port))
+
+    # Set up the development server on port 8000.
+    app.debug = True
+    app.run(port=port)
